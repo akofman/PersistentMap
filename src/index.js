b61cfe880dec1429643abc73c4d33d40e618e3a5
Alexis Kofman <alexis.kofman@gmail.com>
2016-10-03 14:57:59 +0200

    Init project :rocket:

---
+++ src/index.js
@@ -0,0 +1,20 @@
+/**
+*
+* @module PersistentMap
+*/
+
+import create from './create';
+
+class PersistentMap {
+  constructor (name) {
+    create(name);
+  }
+
+  import () {}
+  set () {}
+  get () {}
+  delete () {}
+
+}
+
+export default PersistentMap;
