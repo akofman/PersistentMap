763dda47371333c09d4546d428593ae177b39ee3
Alexis Kofman <alexis.kofman@gmail.com>
2016-10-04 17:21:26 +0200

    Add dist

---
+++ webpack.config.js
@@ -0,0 +1,50 @@
+const webpack = require('webpack');
+// This helps ensure the builds are consistent if source hasn't changed:
+const occurrenceOrderPlugin = new webpack.optimize.OccurrenceOrderPlugin();
+const dedupePlugin = new webpack.optimize.DedupePlugin();
+// Try to dedupe duplicated modules, if any:
+const definePlugin = new webpack.DefinePlugin({
+  __DEV__: JSON.stringify(JSON.parse(process.env.NODE_ENV === 'development' || 'false')),
+  'process.env': {
+    NODE_ENV: JSON.stringify(process.env.NODE_ENV || 'production')
+  }
+});
+// Minify the code.
+const uglifyJsPlugin = new webpack.optimize.UglifyJsPlugin({
+  sourceMap: true,
+  compress: {
+    warnings: false
+  },
+  output: {
+    comments: false
+  }
+});
+
+module.exports = {
+  // Don't attempt to continue if there are any errors.
+  bail: true,
+  entry: [
+    './src'
+  ],
+  devtool: 'source-map',
+  output: {
+    path: 'dist',
+    filename: 'data.js',
+    libraryTarget: 'umd'
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.(js)$/,
+        exclude: /node_modules/,
+        loader: 'babel'
+      }
+    ]
+  },
+  plugins: [
+    definePlugin,
+    occurrenceOrderPlugin,
+    dedupePlugin,
+    uglifyJsPlugin
+  ]
+};
